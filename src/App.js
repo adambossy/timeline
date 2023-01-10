7a4ee8db63fb561949b7bdc8796fa6ac600b0031
Adam Bossy <adambossy@gmail.com>
2023-01-10 15:44:19 -0500

    Add WAY CLEANER version of the vertically cascading algorithm

--- src/App.js
+++ src/App.js
@@ -39,83 +39,82 @@ function computeTicks(startDate, endDate, interval) {
     return Math.round(ticks);
 }
 
-function createGraph(events, eventRefs, startDate, endDate, interval, spaceBetweenTicks) {
-    // TODO figure out how to get all this to compute just once
-    if (events.length === 0)
-        return []
-    let lastBottom = undefined
-    let lastLeft = 0
-    return events.map((e, i) => {
-        const ref = eventRefs.current[i];
-        // console.log(ref.clientWidth + 'x' + ref.clientHeight);
+class EventBox {
+    constructor(event, x, y, width, height) {
+        this.event = event
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+}
 
-        let style = ref.getAttribute('style') // HACK! Appending to getAttribute is temporary in order to support random coloring
-        if (e.date) {
-            const y = monthDiff(startDate, e.date) * (spaceBetweenTicks / interval)
-            style = style + ";top:" + y + "px"
+// TODO generalize to support both horizontal and vertical
+class SimpleVerticalTimeline {
 
-            console.log(e.name + ' y ' + y + ' lastBottom ' + lastBottom)
-            if (y < lastBottom) {
-                const marginLeft = parseInt(window.getComputedStyle(ref).getPropertyValue('margin-left')) // UGLY
-                const marginRight = parseInt(window.getComputedStyle(ref).getPropertyValue('margin-right')) // UGLY
-                lastLeft += ref.clientWidth + marginLeft + marginRight
-                style = style + "; left: " + lastLeft + "px"
-                console.log("successfully shifted right ", lastLeft)
-            } else {
-                lastLeft = 0
-                console.log("reset left")
+    margin = 10
+
+    projectionOverlaps(minA, maxA, minB, maxB) {
+        return maxA >= minB && maxB >= minA
+    }
+
+    isOverlapping(boxA, boxB) {
+
+        return this.projectionOverlaps(boxA.x, boxA.width, boxB.x, boxB.width) &&
+            this.projectionOverlaps(boxA.y, boxA.height, boxB.y, boxB.height)
+    }
+
+    totalDays(startDate, endDate) {
+        return this.dayDiff(startDate, endDate)
+    }
+
+    dayDiff(dateFrom, dateTo) {
+        const diffTime = Math.abs(dateTo - dateFrom);
+        return Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    }
+
+    create(eventBoxes, startDate, endDate, canvasHeight) {
+        let lastBox
+        const totalDays = this.totalDays(startDate, endDate)
+        return eventBoxes.map((b, i) => {
+            const days = this.dayDiff(startDate, b.event.date)
+            b.y = (days / totalDays) * canvasHeight
+
+            if (lastBox && 
+                this.projectionOverlaps(
+                    lastBox.y,
+                    lastBox.y + lastBox.height + this.margin,
+                    b.y,
+                    b.y + b.height + this.margin)) {
+                b.x = lastBox.x + lastBox.width + this.margin
             }
 
-            ref.setAttribute('style', style)
-            lastBottom = y + ref.clientHeight
-        }
-    });
+            lastBox = b
+            return b
+        });
+    }
 }
-/*
-function dayDiff(dateFrom, dateTo) {
-    const diffTime = Math.abs(dateTo - dateFrom);
-    return Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-}
-*/
+
+
 function monthDiff(dateFrom, dateTo) {
     return dateTo.getMonth() - dateFrom.getMonth() +
    (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
 }
-
-const colors = [
-    'red',
-    'green',
-    'blue',
-    'gray'
-]
-
-/*
-function randomColor() {
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    return color
-}
-*/
 
 function App() {
     const startDate = new Date("January 1, 2016");
     const endDate = new Date("December, 2018");
     const interval = 3 // number of months per "tick"
 
-    const canvasHeight = 1200;
+    const canvasHeight = 1500;
     const numTicks = computeTicks(startDate, endDate, interval);
     const spaceBetweenTicks = canvasHeight / numTicks;
 
     const eventRefs = useRef([]);
 
-//    const eventRefs = Array.from(Array(numTicks), (e, i) => {
-//        return useRef(null)
-//    });
-
     state.events.sort((a, b) => {
         const dateA = a.date
-        console.log('dateA', dateA);
         const dateB = b.date
-        console.log('dateB', dateB);
         if (dateA < dateB) {
             return -1
         } else if (dateB < dateA) {
@@ -124,14 +123,30 @@ function App() {
             return 0
         }
     })
-    console.log(state.events)
 
     useEffect(() => {
-        eventRefs.current = eventRefs.current.slice(0, state.events.length);
-        eventRefs.current.map((e, i) => {
-            // console.log(e.clientWidth + 'x' + e.clientHeight);
-        })
-        createGraph(state.events, eventRefs, startDate, endDate, interval, spaceBetweenTicks);
+        // Prep eventBoxes
+        const eventBoxes = state.events.map((e, i) => {
+            const ref = eventRefs.current[i]
+            return new EventBox(e, 0, 0, ref.clientWidth, ref.clientHeight)
+        });
+
+        console.log(new SimpleVerticalTimeline().create(eventBoxes, startDate, endDate, canvasHeight))
+
+        // Apply creation results
+        eventBoxes.map((b, i) => {
+            const ref = eventRefs.current[i]
+            /*
+            const style = {
+                left: b.x,
+                top: b.y
+            }
+            this.setState
+            */
+            const style = "left:" + b.x + "px;top:" + b.y + "px" // HACK
+            ref.setAttribute('style', style)
+            return b
+        });
     })
 
     return (
@@ -144,6 +159,7 @@ function App() {
                     }
 
                     return <div ref={el => eventRefs.current[i] = el}
+                                key={i}
                                 className="Canvas-event">
                         <div className="Canvas-event-name">{e.name}</div>
                         <div className="Canvas-event-date">{date}</div>
