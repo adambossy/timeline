81be802f5a4c76a36ce7f7406f5281986ef62284
Adam Bossy <adambossy@gmail.com>
2023-04-01 17:32:50 -0400

    BIG milestone! Got a recursive data structure and algorithm in place that wosn't trivial to reason about. Have a few small structures being automatically built with high quality

--- src/AppV2.tsx
+++ src/AppV2.tsx
@@ -3,6 +3,147 @@ import 'react-vertical-timeline-component/style.min.css';
 
 import './AppV2.css';
 
+
+interface Event {
+    title: string;
+    date?: Date;
+    startDate?: Date;
+    endDate?: Date;
+}
+
+const singleInstance: Event[] = [
+    {
+        title: "Mantle sweeper",
+        date: new Date("2010-01-01"),
+    },
+]
+
+const singleInstanceGraph: EventGraph = [
+    {
+            title: "Mantle sweeper",
+            date: new Date("2010-01-01"),
+    },
+]
+
+const singleRange: Event[] = [
+    {
+        title: "Sheep groomer",
+        startDate: new Date("2010-01-01"),
+        endDate: new Date("2011-01-01"),
+    },
+]
+
+const twoInstances: Event[] = [
+    {
+        title: "Mantle sweeper",
+        date: new Date("2010-01-01"),
+    },
+    {
+        title: "Plate grower",
+        date: new Date("2011-01-01"),
+    },
+]
+
+const twoInstancesGraph: EventGraph = [
+    {
+        title: "Mantle sweeper",
+        date: new Date("2010-01-01"),
+    },
+    {
+        title: "Plate grower",
+        date: new Date("2011-01-01"),
+    },
+]
+
+const collidingInstances: Event[] = [
+    {
+        title: "Mantle sweeper",
+        date: new Date("2010-01-01"),
+    },
+    {
+        title: "Plate grower",
+        date: new Date("2010-01-01"),
+    },
+]
+
+// type MinLengthArray<T> = [T, T, ...T[]]; // array with two or more elements
+type EventGroup = EventGraph[];
+type EventGraph = (Event | EventGroup)[];
+
+const event1: Event = {
+    title: "Mantle sweeper",
+    date: new Date("2010-01-01"),
+}
+
+const event2: Event = {
+    title: "Plate grower",
+    date: new Date("2010-01-01"),
+}
+
+const group1: EventGroup = [
+    [
+        event1,
+    ],
+    [
+        event2,
+    ]
+]
+
+const collidingInstancesGraph: EventGraph = [
+    group1 
+]
+
+const miniPyramidGraph: EventGraph = [
+    event1,
+    group1 
+]
+
+const collidingInstanceAndRange: Event[] = [
+    {
+        title: "Sheep groomer",
+        startDate: new Date("2010-01-01"),
+        endDate: new Date("2010-03-01"),
+    },
+    {
+        title: "Mantle sweeper",
+        date: new Date("2010-02-01"),
+    },
+]
+
+const collidingInstanceAndRangeGraph: Event[][] = [
+    [
+        {
+            title: "Sheep groomer",
+            startDate: new Date("2010-01-01"),
+            endDate: new Date("2010-03-01"),
+        },
+    ],
+    [
+        {
+            title: "Mantle sweeper",
+            date: new Date("2010-02-01"),
+        },
+    ]
+]
+
+/*
+const events: Events = [
+    {
+        title: "Mantle sweeper",
+        date: new Date("2010-01-01"),
+    },
+    {
+        title: "House of card builder",
+        startDate: new Date("2010-01-01"), // no end date, goes until present
+    },
+    {
+        title: "Sheep groomer",
+        startDate: new Date("2010-01-01"),
+        endDate: new Date("2011-01-01"),
+    },
+]
+*/
+
 enum BubbleSide {
     RIGHT = 'right',
     LEFT = 'left',
@@ -31,6 +172,34 @@ const EventRange: React.FC<EventRangeProps> = ({ height, bubbleSide }) => {
 const EventInstance = () => {
     return (
         <div className="event-instance"></div>
+    )
+}
+
+interface EventGroupProps {
+    group: EventGroup;
+}
+
+const EventGroupComponent: React.FC<EventGroupProps> = ({ group }) => {
+    let sequences = []; // aka "tracks" aka "columns"
+    for (let i = 0; i < group.length; i++) {
+        sequences.push(constructGraph(group[i]));
+    }    
+
+    return (
+        <div className="event-group">
+            <Branch leftChildren={1} rightChildren={1} />
+            <div className="event-sequence-container">
+            {
+                sequences.map((sequence) => {
+                    return (
+                        <div className="event-sequence">
+                            {sequence}
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
     )
 }
 
@@ -80,43 +249,154 @@ const Branch: React.FC<BranchProps> = ({ leftChildren, rightChildren, widthOverr
 }
 
 interface TimelineProps {
+    events?: Event[];
+    graph?: EventGraph; // (Event | Event[])[];
     children: ReactNode;
 }
 
-const Timeline: React.FC<TimelineProps> = ({ children }) => {
-    return <div className="timeline">{children}</div>
+const projectionOverlaps = (minA: number, maxA: number, minB: number, maxB: number) => {
+    return maxA >= minB && maxB >= minA
+}
+
+const buildGraph = (sortedEvents: Event[]) => {
+    let graph = [[sortedEvents[0]]];
+
+/*
+    let active = 0;
+    for (let i = 1; i < sortedEvents.length; i++) {
+        const e1 = sortedEvents[i];
+        for (let j = 1; j < sortedEvents.length; j++) {
+            let index = j + active % sortedEvents.length;
+            const e2 = graph[index];
+            if (!projectionOverlaps(e1.startDate || e1.date, e1.endDate || e1.date, e2.startDate || e2.date, e2.endDate || e2.date) {
+                
+                active = index
+            }
+        }
+    }
+*/
+    return graph;
+}
+
+// type EventGroup = (Event | Event[])[];
+
+const constructGraph = (graph: EventGraph): JSX.Element[] => {
+    let nodes: JSX.Element[] = []
+    for (let i = 0; i < graph.length; i++) {
+        const eventOrGroup = graph[i];
+
+        console.log(`event or graph ${JSON.stringify(eventOrGroup)}`);
+
+        if (!Array.isArray(eventOrGroup)) {
+            console.log(`event`);
+            const event = eventOrGroup as Event
+            if (event.startDate) {
+                nodes.push(<EventRange height={100} bubbleSide={BubbleSide.LEFT} />)
+            } else if (event.date) {
+                nodes.push(<EventInstance />)
+            }
+        }
+
+        if (Array.isArray(eventOrGroup)) {
+            console.log(`graph`);
+            const group = eventOrGroup as EventGroup
+            if (group) {
+                /*
+                let tracks = [];
+                for (let i = 0; i < group.length; i++) {
+                    tracks.push(constructGraph(group[i]));
+                }
+                */
+                nodes.push(<EventGroupComponent group={group} />);
+            }
+        }
+    }
+    return nodes;
+}
+
+const Timeline: React.FC<TimelineProps> = ({ events, graph, children }) => {
+    if ((events === undefined) === (graph === undefined)) {
+        throw new Error("Either `events` or `graph` must be defined, but not both.")
+    }
+
+    if (events) {
+        const sortedEvents = events.sort((a, b) => {
+            const aDate = a.startDate || a.date;
+            const bDate = b.startDate || b.date;
+
+            if (aDate && bDate) {
+                return aDate.getTime() - bDate.getTime();
+            }
+            
+            return 0;
+        });
+
+        // graph = buildGraph(sortedEvents);
+        graph = sortedEvents
+    }
+
+    console.log(`building graph... ${JSON.stringify(graph)}`);
+
+    return <div className="timeline">{graph && constructGraph(graph)}</div>
 }
 
 function AppV2() {
     return (
         <React.Fragment>
+            <Timeline graph={singleInstanceGraph}>
+            </Timeline>
+            <hr/>
+            <Timeline graph={twoInstancesGraph}>
+            </Timeline>
+            <hr/>
+            <Timeline graph={collidingInstancesGraph}>
+            </Timeline>
+            <hr/>
+            <Timeline graph={miniPyramidGraph}>
+            </Timeline>
+            <hr/>
+            {/*
+            <Timeline graph={collidingInstanceAndRangeGraph}>
+            </Timeline>
+            <Timeline events={singleInstance}>
+                <EventInstance />
+            </Timeline>
+            <hr/>
+            <Timeline events={singleRange}>
+                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
+            </Timeline>
+            <hr/>
+            <Timeline events={twoInstances}>
+                <EventInstance />
+                <EventInstance />
+            </Timeline>
+            <hr/>
+            <Timeline events={collidingInstances}>
+                <EventInstance />
+                <EventInstance />
+            </Timeline>
+            <hr/>
+            <Timeline events={collidingInstanceAndRange}>
+                <EventInstance />
+                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
+            </Timeline>
+            {/*
+            <hr/>
             <Timeline>
+                <EventInstance />
+                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
+            </Timeline>
+            <hr/>
+            <Timeline>
+                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
                 <EventInstance />
             </Timeline>
             <hr/>
             <Timeline>
                 <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
-            </Timeline>
-            <hr/>
-            <Timeline>
-                <EventInstance />
-                <EventInstance />
-            </Timeline>
-            <hr/>
-            <Timeline>
-                <EventInstance />
                 <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
             </Timeline>
-            <hr/>
-            <Timeline>
-                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
-                <EventInstance />
-            </Timeline>
-            <hr/>
-            <Timeline>
-                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
-                <EventRange height={100} bubbleSide={BubbleSide.LEFT}  />
-            </Timeline>
+            */}
             <hr/>
             <div className="timeline">
                 <TimelineStem height={20} />
