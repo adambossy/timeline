8b27d4a9ea61ab6d8b65c6a7b879b4c1b96c79fa
Adam Bossy <adambossy@gmail.com>
2023-03-31 23:21:47 -0400

    Start creating componentss. Create components for event ranges, event instances and timeline stems

--- src/AppV2.tsx
+++ src/AppV2.tsx
@@ -1,57 +1,58 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import StarIcon from '@material-ui/icons/Star';
-import SchoolIcon from '@material-ui/icons/School';
 import WorkIcon from '@material-ui/icons/Work';
 
 import './AppV2.css';
 
-function AppV2() {
-    const style = {
-        color: 'white',
-        fontSize: 200
-    };
+interface EventRangeProps {
+    height: number;
+}
 
+const EventRange: React.FC<EventRangeProps> = ({ height }) => {
     return (
-        <div>
-            <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                    date="2011 - present"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                    <p>
-                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                    </p>
-                </VerticalTimelineElement>
-            </VerticalTimeline>
-            <br/>
-            <br/>
-            <div className="timeline">
-                <div className="timeline-stem"></div>
-                <div className="event-group">
-                    <div className="event-group-fork">
-                        <div className="event-group-fork-line-container" style={{ transform: "rotate(45deg)"}}>
-                            <div className="event-group-fork-line"></div>
-                        </div>
-                        <div className="event-group-fork-line-container" style={{ transform: "rotate(135deg)"}}>
-                            <div className="event-group-fork-line"></div>
-                        </div>
+        <div className="event-range" style={{ height: height + "px" }}></div>
+    )
+}
+
+const EventInstance = () => {
+    return (
+        <div className="event-instance"></div>
+    )
+}
+
+interface StemProps {
+    height: number;
+}
+
+const TimelineStem: React.FC<StemProps> = ({ height }) => {
+    return <div className="timeline-stem" style={{ height: height + "px" }}></div>
+}
+
+function AppV2() {
+    return (
+        <div className="timeline">
+            <div className="event-group">
+                <div className="event-group-fork">
+                    <div className="event-group-fork-line-container" style={{ transform: "rotate(45deg)"}}>
+                        <div className="event-group-fork-line"></div>
                     </div>
-                    <div className="event-sequence-container">
-                        <div className="event-sequence">
-                            <div className="event-range" style={{ height: "200px" }}></div>
-                            <div className="event-range" style={{ height: "300px", top: "50px" }}></div>
-                        </div>
-                        <div className="event-sequence">
-                            <div className="event-range" style={{ height: "400px", top: "60px" }}></div>
-                        </div>
+                    <div className="event-group-fork-line-container" style={{ transform: "rotate(135deg)"}}>
+                        <div className="event-group-fork-line"></div>
+                    </div>
+                </div>
+                <div className="event-sequence-container">
+                    <div className="event-sequence">
+                        <TimelineStem height={20} />
+                        <EventRange height={200} />
+                        <TimelineStem height={50} />
+                        <EventRange height={300} />
+                    </div>
+                    <div className="event-sequence">
+                        <TimelineStem height={20} />
+                        <EventRange height={400} />
+                        <TimelineStem height={75} />
+                        <EventInstance />
                     </div>
                 </div>
             </div>
